f401eaa9b89174c088f461dcdc1bbe5c5d69792c
aindrila <aindrila.mukherjee@capgemini.com>
2022-08-10 17:28:34 +0530

    initial commit

--- cmsfrontend/src/app/app.module.ts
+++ cmsfrontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriesComponent } from './categories/categories.component';
@@ -24,6 +25,8 @@ import { UsersComponent } from './users/users.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- cmsfrontend/src/app/login/login.component.ts
+++ cmsfrontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl,FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,18 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 
-  export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit {
 
-    
+  emailRegex : string = '[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}';
+  passwordRegex : string = '^[a-zA-Z0-9]{10}$';
+  validEmail : boolean = true;
+  validPass : boolean = true;
+
+  constructor() { }
+
+
+    emailF = new FormControl('',[Validators.required,Validators.pattern('')]);
+    pass = new FormControl('', [Validators.required,Validators.pattern('')])
+
+
+  email($event: any){
+    this.validEmail = true;
   
-    constructor() { }
-  
-  
-      
-  
-    
-    ngOnInit(): void {
-    }
-  
-    
+
   }
+
+  password($event: any){
+    this.validPass = true;
+  }
+
+  ngOnInit(): void {
+  }
+
+  signIn($event:any){
+  if(this.emailF.value === 'aaa@gmail.com'){
+       this.validEmail = true;
+   }
+    else{
+      this.validEmail = false;
+   }
+   if(this.pass.value === 'abcde12345'){
+     this.validPass = true;
+    }
+    else{
+      this.validPass = false;
+     }
+     console.log(this.validEmail,' ',this.validPass);
+ }
+}
